3f227178f6295e4df0b0671c35f9c63ab4dd6456
kilianstallz <mobil.kilian@live.com>
2018-12-18 21:50:54 +0100

    Style changes, added header bg, hire me section

--- _javascript/main.js
+++ _javascript/main.js
@@ -14,6 +14,7 @@ $(document).ready(function() {
     if ($(this).scrollTop() > 200) {
        $('.navbar').removeClass('is-white').addClass('is-primary')
        $('.navbar-item').addClass('has-text-white')
+       $('.name').addClass('has-text-black')
     } else {
       $('.navbar').addClass('is-white').removeClass('is-primary')
       $('.navbar-item').removeClass('has-text-white')
